094da699ebf2724349b2f457214d00ad4beb89da
hwjoo <jhw664089@gmail.com>
2025-02-26 13:01:12 +0900

    feat: init project

---
+++ src/main/java/me/hwjoo/feedback_survey/FeedbackSurveyApplication.java
@@ -0,0 +1,13 @@
+package me.hwjoo.feedback_survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackSurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hwjoo/feedback_survey/FeedbackSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwjoo.feedback_survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
